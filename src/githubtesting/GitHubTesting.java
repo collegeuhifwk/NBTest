022b7fc7a0a1c982700acbd21420323a06b90e96
Finlay Watson-Kerr 13011450 <13011450@uhi.ac.uk>
2019-07-17 20:31:33 +0100

    Initial commit

---
+++ src/githubtesting/GitHubTesting.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtesting;
+
+/**
+ *
+ * @author fwk
+ */
+public class GitHubTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
